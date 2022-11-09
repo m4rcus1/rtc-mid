fb5bff52e7984fb98fde400c7e9702f2f836c37f
m4rcus1 <anhq6009@gmail.com>
2022-11-09 10:54:22 +0700

    first commit

---
+++ public/script.js
@@ -0,0 +1,72 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video_call')
+const peer = new Peer(undefined)
+const myVideo = document.createElement('video')
+const mySpan = document.createElement('span')
+let u_id = 1
+myVideo.muted = true
+const peers = {}
+let check = []
+console.log(id)
+
+socket.on('user-connected', userId => {
+    console.log("user id:" + userId)
+})
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addvideo(mySpan, myVideo, stream)
+    peer.on('call', call => {
+        //answer the call 
+        call.answer(stream);
+        const video = document.createElement('video')
+        const span = document.createElement('span')
+        call.on('stream', function (userVideoStream) {
+            addvideo(span, video, userVideoStream)
+        })
+    })
+    socket.on('user-connected', userId => {
+        //send a message to server that i am new user
+        connectToNewUser(userId, stream)
+    })
+})
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+    check.pop(userId)
+})
+
+peer.on('open', id1 => {
+    socket.emit('join-room', id, id1)
+})
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream)//create a call
+    const span= document.createElement('span')
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      // add new vieo
+      addvideo(span,video, userVideoStream)
+    })
+    call.on('close', () => {
+      //when close remove
+      span.remove()
+      video.remove()
+    })
+  
+    peers[userId] = call
+  }
+function addvideo(sp, video, stream) {
+    x = (check.includes(stream['id']))
+    if (!x) {
+        check.push(stream['id'])
+        video.srcObject = stream
+        video.addEventListener('loadedmetadata', () => {
+            video.play()
+        })
+        sp.innerHTML = 'User number ' + (check.indexOf(stream['id']) + 1)
+        sp.id = stream['id']
+        u_id++;
+        videoGrid.append(sp)
+        sp.append(video);
+    }
+}
